218b89e102b0af4683de75bd4d3704f7e32619d2
Ben <1913889277@qq.com>
2015-12-17 12:20:13 +0800

    First commit

---
+++ src/com/coolweather/app/util/Utility.java
@@ -0,0 +1,81 @@
+package com.coolweather.app.util;
+
+import com.coolweather.app.db.CoolWeatherDB;
+import com.coolweather.app.model.City;
+import com.coolweather.app.model.Country;
+import com.coolweather.app.model.Province;
+
+import android.text.TextUtils;
+
+//由于服务器返回的省市县数据都是“代号|城市，代号|城市”这种格式，所以最好在提供一个工具类解析和处理这种数据
+//解析的规则是先按,分隔，再按|分隔，接着讲解析出来的数据设置到实体类中，最后调用CoolWeatherDB中的三个save()方法将数据存储到相应的表中
+public class Utility {
+	/**
+	 * 解析和处理服务器返回的省级数据
+	 * */
+	public synchronized static boolean handleProvinceResponse(
+			CoolWeatherDB coolWeatherDB, String response) {
+		if (!TextUtils.isEmpty(response)) {
+			String[] allProvinces = response.split(",");
+			if (allProvinces != null && allProvinces.length > 0) {
+				for (String p : allProvinces) {
+					String[] array = p.split("\\|");
+					Province province = new Province();
+					province.setProvinceName(array[0]);
+					province.setProvinceCode(array[1]);
+					// 将解析出来的数据储存到Province表
+					coolWeatherDB.saveProvince(province);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 解析和处理服务器返回的市级数据
+	 * */
+	public static boolean handleCitiesResponse(
+			CoolWeatherDB coolWeatherDB, String response, int provinceId) {
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCities = response.split(",");
+			if (allCities != null && allCities.length > 0) {
+				for (String c : allCities) {
+					String[] array = c.split("\\|");
+					City city = new City();
+					city.setCityName(array[0]);
+					city.setCityName(array[1]);
+					city.setProvinceId(provinceId);
+					// 将解析出来的数据储存到City表
+					coolWeatherDB.saveCity(city);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 解析和处理服务器返回的县级数据
+	 * */
+	public static synchronized boolean handleCountriesResponse(
+			CoolWeatherDB coolWeatherDB, String response, int cityId) {
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCountries = response.split(",");
+			if (allCountries != null && allCountries.length > 0) {
+				for (String c : allCountries) {
+					String[] array = c.split(",");
+					Country country = new Country();
+					country.setCountryName(array[0]);
+					country.setCountryCode(array[1]);
+					country.setCityId(cityId);
+					// 将解析出来的数据储存到Country表
+					coolWeatherDB.saveCountry(country);
+
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+}
